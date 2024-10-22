848d0f7007a773d6f66ba3cb307b558dcc55fa78
engrmazharjan <mazharj4565@gmail.com>
2024-10-22 22:40:16 +0500

    Initial project setup

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="text-[#1e1e1e] text-[14px]">Footer</div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className="text-[#1e1e1e] text-[14px]">Header</div>
+    )
+}
+
+export default Header;
